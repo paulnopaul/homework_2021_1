ab9134932cf20eb42dc5850aaf18ff048d27a75a
Pavel Cheklin <tscheklin@gmail.com>
2021-02-10 20:25:53 +0300

    Исправил имя словаря

--- source/roman.js
+++ source/roman.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const R_TO_D = {
+const ROMAN_TO_DECIMAL = {
 	M: 1000, CM: 900, D: 500, CD: 400,
 	C: 100, XC: 90, L: 50,
 	XL: 40, X: 10, IX: 9,
@@ -10,10 +10,10 @@ const R_TO_D = {
 const decimalToRoman = function (num) {
 	num = parseInt(num);
 	let res = '';
-	for (let i in R_TO_D) {
-		while (num >= R_TO_D[i]) {
+	for (let i in ROMAN_TO_DECIMAL) {
+		while (num >= ROMAN_TO_DECIMAL[i]) {
 			res += i;
-			num -= R_TO_D[i];
+			num -= ROMAN_TO_DECIMAL[i];
 		}
 	}
 	return res;
@@ -24,7 +24,7 @@ const romanToDecimal = function (num) {
 	let res = 0;
 	let prev = 1001;
 	for (let i = 0; i < num.length; ++i) {
-		let current = R_TO_D[num[i]];
+		let current = ROMAN_TO_DECIMAL[num[i]];
 		res += current;
 		if (current > prev) {
 			res -= prev * 2;
